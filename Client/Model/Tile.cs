536ca93ee856c20b9891e34137e487051e08abcd
Zuelatak <zuelatak@gmail.com>
2017-11-15 13:45:39 -0700

    Got Solution setup. Left off on Tile Class.

---
+++ Client/Model/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Model
+{
+    class Tile
+    {
+        public Tile(string top, string left, string right, string bot)
+        {
+            
+        }
+    }
+}
+
