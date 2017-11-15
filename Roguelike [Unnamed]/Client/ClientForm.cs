52ccb298a8a45ae474b56185a02ec298a218999c
Zuelatak <zuelatak@gmail.com>
2017-11-15 16:23:25 -0700

    Created Tile Class and can load up the image on the form.

---
+++ Roguelike [Unnamed]/Client/ClientForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+
+namespace Roguelike__Unnamed_
+{
+    public partial class ClientForm : Form
+    {
+        private World m;
+        public ClientForm()
+        {
+            InitializeComponent();
+            m = new World();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            PointF Location = new PointF(125, 175);
+            m.k.Roate();
+            e.Graphics.DrawImage(m.k.image, Location);
+        }
+    }
+}
---
+++ Roguelike [Unnamed]/Model/Tile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace Model
+{
+    public class Tile
+    { 
+        public Image image;
+        public bool top;
+        public bool left;
+        public bool right;
+        public bool bot;
+        public Tile(string tileName)
+        {
+            if (tileName == "2Way")
+            {
+                image = Image.FromFile("../../../Resources/Images/2-way.jpg");
+                top = true;
+                left = false;
+                right = false;
+                bot = true;
+            }
+            else if (tileName == "3Way")
+            {
+                image = Image.FromFile("../../../Resources/Images/3-way.jpg");
+                top = true;
+                left = true;
+                right = false;
+                bot = true;
+            }
+            else if (tileName == "4Way")
+            {
+                image = Image.FromFile("../../../Resources/Images/4-way.jpg");
+                top = true;
+                left = true;
+                right = true;
+                bot = true;
+            }
+            else if (tileName == "LTile")
+            {
+                image = Image.FromFile("../../../Resources/Images/L.jpg");
+                top = true;
+                left = true;
+                right = false;
+                bot = false;
+            }
+            else if (tileName == "EndTile")
+            {
+                image = Image.FromFile("../../../Resources/Images/End.jpg");
+                top = true;
+                left = false;
+                right = false;
+                bot = false;
+            }
+        }
+        public void Roate()
+        {
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            bool tempTop = top;
+            bool tempLeft = left;
+            bool tempRight = right;
+            bool tempBot = bot;
+            right = tempBot;
+            bot = tempLeft;
+            left = tempTop;
+            top = tempRight;
+        }
+    }
+}
+
